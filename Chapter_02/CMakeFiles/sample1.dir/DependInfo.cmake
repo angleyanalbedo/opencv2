db14c20a92b5314914e627927eacb4f776544d45
angleyanalbedo <100198247+angleyanalbedo@users.noreply.github.com>
2023-03-05 16:40:41 +0800

    update

---
+++ Chapter_02/CMakeFiles/sample1.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/d/BaiduNetdiskDownload/Learn-OpenCV-4-By-Building-Projects-Second-Edition-master/Learn-OpenCV-4-By-Building-Projects-Second-Edition-master/Chapter_02/sample1.cpp" "CMakeFiles/sample1.dir/sample1.cpp.obj" "gcc" "CMakeFiles/sample1.dir/sample1.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Chapter_02/CMakeFiles/sample1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sample1.dir/sample1.cpp.obj"
+  "CMakeFiles/sample1.dir/sample1.cpp.obj.d"
+  "sample1"
+  "sample1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sample1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
